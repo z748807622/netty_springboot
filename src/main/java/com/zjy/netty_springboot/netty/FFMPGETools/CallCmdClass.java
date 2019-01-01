ddbd5419d4d6877ba2bff9b136606741892ea409
ZJY <748807622@qq.com>
2019-01-01 20:01:34 +0800

    cmd调用类

--- src/main/java/com/zjy/netty_springboot/netty/FFMPGETools/CallCmdClass.java
+++ src/main/java/com/zjy/netty_springboot/netty/FFMPGETools/CallCmdClass.java
@@ -1,35 +1,50 @@
 package com.zjy.netty_springboot.netty.FFMPGETools;
 
+import io.netty.channel.group.ChannelGroup;
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-public class MyTest {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        /*ProcessBuilder builder = new ProcessBuilder();
-        builder.command("ping 127.0.0.1");
-        builder.redirectErrorStream();
-        Process proc = builder.start();
-        BufferedReader stdout = new BufferedReader(new InputStreamReader(proc.getInputStream(),"GBK"));
-        BufferedReader errout = new BufferedReader(new InputStreamReader(proc.getErrorStream(),"GBK"));
+public abstract class CallCmdClass {
 
-        String line;
-        while ((line = stdout.readLine()) != null){
-            System.out.println(line);
+    public Process p;
+
+    /**
+     * 发送cmd的错误信息
+     * @param channels
+     * @param error
+     */
+    public abstract void sendErrorInfo(ChannelGroup channels, String error);
+
+    /**
+     * 发送cmd的日志信息
+     * @param channels
+     * @param log
+     */
+    public abstract void sendLogInfo(ChannelGroup channels, String log);
+
+    /**
+     * 关闭cmd
+     */
+    public void close(){
+        if(p != null){
+            p.destroy();
         }
-        proc.waitFor();
-        stdout.close();*/
-        new MyTest().execCmd();
     }
 
-    public void execCmd(){
+    /**
+     * 调用cmd命令
+     * @param channels
+     * @param cmd
+     */
+    public void callCmd(ChannelGroup channels, String cmd){
         try {
             //Process p = Runtime.getRuntime().exec("ping 172.247.34.70");
-            String cmd = "ffmpeg -stream_loop -1 -re -i D:/video/哈利波特第8部哈利波特与死亡圣器(下).mp4 -f mpegts -codec:v mpeg1video -s 864*486 udp://127.0.0.1:4444";
+            //String cmd = "ffmpeg -stream_loop -1 -re -i D:/video/哈利波特第8部哈利波特与死亡圣器(下).mp4 -f mpegts -codec:v mpeg1video -s 864*486 udp://127.0.0.1:4444";
             //String cmd2 = "ffmpeg";
             String cmd3 = "ping 172.247.34.70";
-            Process p = Runtime.getRuntime().exec(cmd);
+            p = Runtime.getRuntime().exec(cmd);
 
             new Thread(()->{
                 try {
@@ -37,7 +52,8 @@ public class MyTest {
                     BufferedReader reader = new BufferedReader(new InputStreamReader(is, "GBK"));
                     String line;
                     while ((line = reader.readLine()) != null) {
-                        System.out.println("info--"+line);
+                        //System.out.println("info--"+line);
+                        sendLogInfo(channels,line);
                     }
                     is.close();
                     reader.close();
@@ -52,7 +68,8 @@ public class MyTest {
                     BufferedReader reader = new BufferedReader(new InputStreamReader(is, "GBK"));
                     String line;
                     while ((line = reader.readLine()) != null) {
-                        System.out.println("error -- "+line);
+                        //System.out.println("error -- "+line);
+                        sendErrorInfo(channels, line);
                     }
                     is.close();
                     reader.close();
